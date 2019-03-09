18f60a438aa56ba3aaa0372600a9381f81418a16
JoseLuisOliveiraCunha <zeluiscunha@outlook.com>
2019-03-09 18:55:24 +0000

    GRANDE commit

--- helloworld.ts
+++ helloworld.ts
@@ -1,17 +1,122 @@
-/// <reference path="interpreter.ts" />
-namespace InterpreterPattern {
+// /// <reference path="interpreter.ts" />
+// import {InstructionExpression} from './interpreter'
 
-    export function test(str : String){
-        var instruction : InstructionExpression = new InstructionExpression();
-        return instruction.interpret(str);
-    }
-
-    function greeter(person) {
-        return "Hello, " + person;
-    }
-
-    let user = "Jane User";
-
-    document.body.innerHTML = String(test("draw square 2 2 4"));
-
+interface AbstractExpression {
+    interpret(context: String): boolean;
 }
+
+class TemporaryExpression implements AbstractExpression {
+    
+    constructor(){}
+
+    interpret(context: String): boolean {
+        return false;
+    }
+}
+
+class MasterExpression implements AbstractExpression {
+
+    private expression : AbstractExpression;
+
+    constructor() {
+        this.expression = new TemporaryExpression();
+    }
+
+    public interpret(context: String): boolean {
+        console.log("`interpret` method of MasterExpression is being called!");
+
+        var contextParts : String[] = context.split(" AND ");
+
+        if(contextParts.length == 1) {
+            this.expression = new InstructionExpression();
+        }
+        else {
+            this.expression = new AndExpression();
+        }
+
+        return this.expression.interpret(context);
+    }
+}
+
+class InstructionExpression implements AbstractExpression {
+
+    private expression : AbstractExpression; 
+
+    constructor(){
+        this.expression = new TemporaryExpression();
+    }
+
+    public interpret(context: String): boolean {
+        console.log("`interpret` method of InstructionExpression is being called!");
+
+        var contextParts : String[] = context.split(" ");
+
+        switch(contextParts[0])
+        {
+            case "draw":
+                this.expression = new DrawExpression();
+                break;
+            default: 
+                return false;
+    
+        }
+        
+        var rest : String = context.substring(context.indexOf(" ") + 1);
+        
+        return this.expression.interpret(rest);
+    }
+}
+
+class AndExpression implements AbstractExpression {
+
+    private lhsExpression : AbstractExpression;
+    private rhsExpression : AbstractExpression;
+
+    constructor(){
+        this.lhsExpression = new TemporaryExpression();
+        this.rhsExpression = new TemporaryExpression();
+    }
+
+    public interpret(context: String): boolean {
+        console.log("`interpret` method of AndExpression is being called!");
+
+        var contextParts : String[] = context.split(" AND ");
+        var restContext : String = context.substr(context.indexOf(" AND ") + " AND ".length);
+
+        this.lhsExpression = new InstructionExpression();
+        this.rhsExpression = new MasterExpression();
+
+        return this.lhsExpression.interpret(contextParts[0]) && this.rhsExpression.interpret(restContext);
+    }
+}
+
+class DrawExpression implements AbstractExpression {
+
+    public interpret(context: String): boolean {
+        console.log("`interpret` method of DrawExpression is being called!");
+
+        var contextParts : String[] = context.split(" ");
+
+        switch(contextParts[0])
+        {
+            case "square":
+                console.log("DRAW SQUARE" + context.substr(context.indexOf(" ")));
+                return contextParts.length == 4;
+                break;
+            case "rect":
+                console.log("DRAW RECT");
+                return contextParts.length == 5;
+                break;
+            default: 
+                return false;
+        }
+    }
+}
+
+function test(str : String){
+    var instruction : MasterExpression = new MasterExpression();
+    return instruction.interpret(str);
+}
+
+document.body.innerHTML = String(test("draw square 2 2 4"));
+
--- interpreter.ts
+++ interpreter.ts
@@ -1,77 +1,73 @@
 //AND e UNION operator proxima aula 
 
-namespace InterpreterPattern {
 
-    export interface AbstractExpression {
-        interpret(context: String): boolean;
+/* export interface AbstractExpression {
+    interpret(context: String): boolean;
+}
+
+export class InstructionExpression implements AbstractExpression {
+
+    private expression: AbstractExpression;
+
+    constructor() {
+        this.expression = new InstructionExpression();
     }
 
-    export class InstructionExpression implements AbstractExpression {
+    public interpret(context: String): boolean {
+        console.log("`interpret` method of InstructionExpression is being called!");
 
-        private expression : AbstractExpression; 
+        var contextParts: String[] = context.split(" ");
 
-        constructor(){
+        switch (contextParts[0]) {
+            case "draw":
+                this.expression = new DrawExpression();
+                break;
+            default:
+                return false;
 
         }
 
-        public interpret(context: String): boolean {
-            console.log("`interpret` method of InstructionExpression is being called!");
+        var rest: String = context.substring(context.indexOf(" "));
 
-            var contextParts : String[] = context.split(" ");
-
-            switch(contextParts[0])
-            {
-                case "draw":
-                    this.expression = new DrawExpression();
-                    break;
-                default: 
-                    return false;
-     
-            }
-            
-            var rest : String = context.substring(context.indexOf(" "));
-            
-            return this.expression.interpret(rest);
-        }
-    }
-
-    export class AndExpression implements AbstractExpression {
-
-        private expression1 : AbstractExpression;
-        private expression2 : AbstractExpression;
-
-        constructor(e1 : AbstractExpression , e2 : AbstractExpression){
-            this.expression1 = e1;
-            this.expression2 = e2;
-        }
-
-        public interpret(context: String): boolean {
-            console.log("`interpret` method of DrawExpression is being called!");
-
-            return this.expression1.interpret(context) && this.expression2.interpret(context);
-        }
-    }
-
-    export class DrawExpression implements AbstractExpression {
-
-        public interpret(context: String): boolean {
-            console.log("`interpret` method of DrawExpression is being called!");
-
-            var contextParts : String[] = context.split(" ");
-
-            switch(contextParts[0])
-            {
-                case "square":
-                    console.log("DRAW SQUARE");
-                    return contextParts.length == 4;
-                    break;
-                case "rect":
-                    console.log("DRAW SQUARE");
-                    return contextParts.length == 5;
-                    break;
-                default: 
-                    return false;
-            }
-        }
+        return this.expression.interpret(rest);
     }
 }
+
+export class AndExpression implements AbstractExpression {
+
+    private expression1: AbstractExpression;
+    private expression2: AbstractExpression;
+
+    constructor(e1: AbstractExpression, e2: AbstractExpression) {
+        this.expression1 = e1;
+        this.expression2 = e2;
+    }
+
+    public interpret(context: String): boolean {
+        console.log("`interpret` method of DrawExpression is being called!");
+
+        return this.expression1.interpret(context) && this.expression2.interpret(context);
+    }
+}
+
+export class DrawExpression implements AbstractExpression {
+
+    public interpret(context: String): boolean {
+        console.log("`interpret` method of DrawExpression is being called!");
+
+        var contextParts: String[] = context.split(" ");
+
+        switch (contextParts[0]) {
+            case "square":
+                console.log("DRAW SQUARE");
+                return contextParts.length == 4;
+                break;
+            case "rect":
+                console.log("DRAW SQUARE");
+                return contextParts.length == 5;
+                break;
+            default:
+                return false;
+        }
+    }
+} */
