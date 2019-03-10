0ab65b283f6fed414f142584162487bcfc4ff622
André Baptista <aca.baptista@gmail.com>
2019-03-10 01:22:22 +0000

    draw square working on canvastool, not yet on svg

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -2,7 +2,7 @@
 // import {InstructionExpression} from './interpreter'
 
 interface AbstractExpression {
-    interpret(context: String): boolean;
+    interpret(context: String, tool: DrawTool): boolean;
 }
 
 class TemporaryExpression implements AbstractExpression {
@@ -22,7 +22,7 @@ class MasterExpression implements AbstractExpression {
         this.expression = new TemporaryExpression();
     }
 
-    public interpret(context: String): boolean {
+    public interpret(context: String, tool: DrawTool): boolean {
         console.log("`interpret` method of MasterExpression is being called!");
 
         var contextParts : String[] = context.split(" AND ");
@@ -34,7 +34,7 @@ class MasterExpression implements AbstractExpression {
             this.expression = new AndExpression();
         }
 
-        return this.expression.interpret(context);
+        return this.expression.interpret(context, tool);
     }
 }
 
@@ -46,7 +46,7 @@ class InstructionExpression implements AbstractExpression {
         this.expression = new TemporaryExpression();
     }
 
-    public interpret(context: String): boolean {
+    public interpret(context: String, tool: DrawTool): boolean {
         console.log("`interpret` method of InstructionExpression is being called!");
 
         var contextParts : String[] = context.split(" ");
@@ -63,7 +63,7 @@ class InstructionExpression implements AbstractExpression {
         
         var rest : String = context.substring(context.indexOf(" ") + 1);
         
-        return this.expression.interpret(rest);
+        return this.expression.interpret(rest, tool);
     }
 }
 
@@ -77,7 +77,7 @@ class AndExpression implements AbstractExpression {
         this.rhsExpression = new TemporaryExpression();
     }
 
-    public interpret(context: String): boolean {
+    public interpret(context: String, tool: DrawTool): boolean {
         console.log("`interpret` method of AndExpression is being called!");
 
         var contextParts : String[] = context.split(" AND ");
@@ -86,13 +86,13 @@ class AndExpression implements AbstractExpression {
         this.lhsExpression = new InstructionExpression();
         this.rhsExpression = new MasterExpression();
 
-        return this.lhsExpression.interpret(contextParts[0]) && this.rhsExpression.interpret(restContext);
+        return this.lhsExpression.interpret(contextParts[0], tool) && this.rhsExpression.interpret(restContext, tool);
     }
 }
 
 class DrawExpression implements AbstractExpression {
 
-    public interpret(context: String): boolean {
+    public interpret(context: String, tool: DrawTool): boolean {
         console.log("`interpret` method of DrawExpression is being called!");
 
         var contextParts : String[] = context.split(" ");
@@ -101,6 +101,7 @@ class DrawExpression implements AbstractExpression {
         {
             case "square":
                 console.log("DRAW SQUARE" + context.substr(context.indexOf(" ")));
+                tool.drawSquare(contextParts);
                 return contextParts.length == 4;
                 break;
             case "rect":
@@ -113,33 +114,46 @@ class DrawExpression implements AbstractExpression {
     }
 }
 
-class Commands {
+interface DrawTool {
+    drawSquare(args: String[]): any;
+}
 
-    constructor(){}
+class CanvasTool implements DrawTool {
 
-    public test(str : String){
-        var instruction : MasterExpression = new MasterExpression();
-        return instruction.interpret(str);
-    }
+    private canvas = <HTMLCanvasElement> document.getElementById('canvas');
     
-    public draw() {
-        var canvas = <HTMLCanvasElement> document.getElementById('canvas');
-        console.log("antes ");
-        if (canvas.getContext) {
-            var ctx = <CanvasRenderingContext2D> canvas.getContext('2d');
-      
-            ctx.fillRect(25, 25, 100, 100);
-            console.log("AAA");
-        }
+    drawSquare(args: String[]) {
+        
+        args.shift() //to remove the 'square' argument of the draw expression
+
+        var canvasContext = <CanvasRenderingContext2D> this.canvas.getContext('2d');
+
+        let x = Number(args[0]);
+        let y = Number(args[1]);
+        let size = Number(args[2]);
+
+        canvasContext.fillRect(x, y, size, size);
     }
+
 }
 
-window.onload = () => {
-	let app = new Commands();
+class SVGTool implements DrawTool {
+    drawSquare(args: String[]) {
+        throw new Error("Method not implemented.");
+    }
 
-    app.draw();
-    document.body.innerHTML = String(app.test("draw square 2 2 4"));
 }
+
+function test(str : String){
+    
+    var instruction : MasterExpression = new MasterExpression();
+    var tool : DrawTool = new CanvasTool();
+    
+    return instruction.interpret(str, tool);
+}
+
+//document.body.innerHTML = String(test("draw square 2 2 4"));
+
 
 
 
