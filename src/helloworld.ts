94e3758b135e0758add1aa107e512ab98bd16dc3
André Baptista <aca.baptista@gmail.com>
2019-03-10 03:28:53 +0000

    draw square/rect canvas and svg
    fully functional, 100% working 2019 no virus money back guaranteed

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -126,7 +126,7 @@ class CanvasTool implements DrawTool {
         
         args.shift() //to remove the 'square' argument of the draw expression
 
-        console.log("DRAW SQUARE " + args);
+        console.log("Canvas - DRAW SQUARE " + args);
 
         let canvasContext = <CanvasRenderingContext2D> this.canvas.getContext('2d');
 
@@ -143,7 +143,7 @@ class CanvasTool implements DrawTool {
         
         args.shift() //to remove the 'rect' argument of the draw expression
 
-        console.log("DRAW SQUARE " + args);
+        console.log("Canvas - DRAW RECT " + args);
 
         let canvasContext = <CanvasRenderingContext2D> this.canvas.getContext('2d');
 
@@ -160,11 +160,52 @@ class CanvasTool implements DrawTool {
 }
 
 class SVGTool implements DrawTool {
+
+    private svg = document.getElementById('svg'); 
+    
+    private namespace = "http://www.w3.org/2000/svg";
+
     drawSquare(args: String[]) {
-        throw new Error("Method not implemented.");
+
+        args.shift() //to remove the 'square' argument of the draw expression
+
+        console.log("SVG - DRAW SQUARE " + args);
+ 
+        let square = document.createElementNS(this.namespace, 'rect');
+
+        let x = <string> args[0];
+        let y = <string> args[1];
+        let size = <string> args[2];
+
+        square.setAttributeNS(null, 'x', x);
+        square.setAttributeNS(null, 'y', y);
+        square.setAttributeNS(null, 'width', size);
+        square.setAttributeNS(null, 'height', size);
+
+        if(this.svg != undefined)
+            this.svg.appendChild(square);
     }
+
     drawRectangle(args: String[]) {
-        throw new Error("Method not implemented.");
+        
+        args.shift() //to remove the 'rect' argument of the draw expression
+
+        console.log("SVG - DRAW RECT " + args);
+ 
+        let rect = document.createElementNS(this.namespace, 'rect');
+
+        let x = <string> args[0];
+        let y = <string> args[1];
+        let width = <string> args[2];
+        let height = <string> args[3];
+
+        rect.setAttributeNS(null, 'x', x);
+        rect.setAttributeNS(null, 'y', y);
+        rect.setAttributeNS(null, 'width', width);
+        rect.setAttributeNS(null, 'height', height);
+
+        if(this.svg != undefined)
+            this.svg.appendChild(rect);
     }
 
 }
@@ -173,11 +214,10 @@ function test(str : String){
     
     var instruction : MasterExpression = new MasterExpression();
     var tool : DrawTool = new CanvasTool();
+    //var tool : DrawTool = new SVGTool();
     
     return instruction.interpret(str, tool);
 }
-
-//document.body.innerHTML = String(test("draw square 2 2 4"));
 
 
 
