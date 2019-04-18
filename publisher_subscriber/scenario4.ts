a0913b8a276ad18e7ea45693278ef933c9231b92
Nelson Almeida <up201505394@fe.up.pt>
2019-04-18 20:20:46 +0100

    scenario 4 implemented

--- publisher_subscriber/scenario4.ts
+++ publisher_subscriber/scenario4.ts
@@ -2,29 +2,70 @@ class Publisher4 {
 
     public queue : BoundedAsyncQueue;
 
-    constructor(queue : BoundedAsyncQueue){
-        this.queue = queue;
+    constructor(maxQueueSize : number){
+        this.queue = new BoundedAsyncQueue(maxQueueSize);
     }
 
     async write(message : Message4){
         await this.queue.enqueue(message);
-        console.log(message.content + " - written to queue");
+        console.log(message.content + " - published (tag: " + message.tag + ")");
     }
-
 }
 
 class Subscriber4 {
 
     public tags : Array<number>;
+    public queue : BoundedAsyncQueue;
 
-    constructor(tags : Array<number>){
+    constructor(tags : Array<number>, maxQueueSize: number){
         this.tags = tags;
+        this.queue = new BoundedAsyncQueue(maxQueueSize);
     }
 
+    async read(){
+        let message = await this.queue.dequeue("subscriber");
+        console.log(message.content + " - read (tag: " + message.tag + ")");
+    }
 }
 
 class Broker {
+    private publishers: Array<Publisher4>;
+    private subscribers: Array<Subscriber4>;
 
+    constructor(publishers: Array<Publisher4>, subscribers: Array<Subscriber4>) {
+        this.publishers = publishers;
+        this.subscribers = subscribers;
+    }
+
+    public async deliverMessage(promise : Promise<Message4>) {
+        let message = await promise;
+        console.log("Broker read: " + message.content + ", " + message.tag);
+        for(let sub of this.subscribers){
+            if(sub.tags.includes(message.tag)){
+                sub.queue.enqueue(message);
+            }
+        }
+
+    }
+
+    public async work() {
+        while(true) {
+            //console.log("Fetching promises from publishers");
+            for(let pub of this.publishers)
+            {
+                let promise = pub.queue.dequeue("broker");
+                this.deliverMessage(promise);
+            }
+
+            let delayPromise = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    //console.log("Broker is resting for 1 sec");
+                    resolve();
+                }, 1000);
+            });
+            await delayPromise;
+        }
+    }
 }
 
 class Message4 {
@@ -46,21 +87,23 @@ class BoundedAsyncQueue {
         this.queue = new Array<Message4>();
         this.semaphore = new AsyncSemaphore (size);
     }
-    
-    // private unresolvedPromisses : Array();
 
     async enqueue(message: Message4) {
-        console.log("checking if enqueue is possible");
+        //console.log("checking if enqueue is possible");
         await this.semaphore.waitPush();
-        console.log("enqueue will proceed");
+        //console.log("enqueue will proceed");
         this.queue.push(message);
         this.semaphore.signalPush();
-        console.log(this.queue);
+        //console.log(this.queue);
     }
 
-    dequeue(): Message4 {
-        this.semaphore.waitPull();
-        let message = this.queue.shift() || new Message4(1, "");
+    async dequeue(order : String): Promise<Message4> {
+        //console.log("checking if dequeue is possible");
+        await this.semaphore.waitPull(order);
+
+        //console.log("DEQUEUE ordered by " + order + ", current );
+        let message = this.queue.shift() || new Message4(1, "error message");
+        //console.log("DEQUED MESSAGE: (" + message.content + ", " + message.tag + "), ordered by " + order + ", remaining queue: " + this.queue);
         this.semaphore.signalPull();
         return message;
     }
@@ -69,52 +112,87 @@ class BoundedAsyncQueue {
 class AsyncSemaphore  {
     
     public currentSize : number;
-    public freeSpace : number;
+    public emptySize : number;
+    public maxSize : number;
 
     constructor(size : number){
         this.currentSize = 0;
-        this.freeSpace = size;
+        this.emptySize = size;
+        this.maxSize = size;
     }
 
     signalPush(): void {
-        this.currentSize++;
-        console.log("Push has been made - queue size has been incremented");
+        this.emptySize--;
+        //console.log("Push has been made - queue size has been incremented");
     }
 
     signalPull(): void {
         this.currentSize--;
+        //console.log("Pull has been made - queue size has been diminished");
     } 
 
     async waitPush(): Promise<void> {
-        // Sinalize that we want to push by reserving a free space
-        this.freeSpace--;
-        console.log("Push intention has been made");
-        console.log("current free space (includes intentions) " + this.freeSpace);
-
-        while(this.freeSpace < 0) { 
-        // Menor do que zero porque o freeSpace já tem em conta a nossa intenção de push
-            console.log("waiting for free space to push message")
-            var promise = new Promise((resolve, reject) => {
+        while(this.currentSize >= this.maxSize) { 
+            //console.log("waiting for free space to push message");
+            let promise = new Promise((resolve, reject) => {
                 setTimeout(() => {
-                    console.log("done waiting for one second"); 
+                    //console.log("done waiting for free space for one second");
                     resolve();
                 }, 1000);
             });
             await promise;
         }
+        this.currentSize++;
     }
 
-    async waitPull(): Promise<void> {
-        //TODO: reproduce the behaviour from waitPush (ver o exemplo da wikipedia - é importante garantir que o primeiro a ser atualizado é currentSize, e não o freeSpace)
-        while(this.currentSize == 0) {
-
-            console.log("waiting for message to pull")
-            let promise = new Promise((resolve, reject) => {
-                setTimeout(() => resolve(), 1000)
+    async waitPull(order: String): Promise<void> {
+        while(this.emptySize >= this.maxSize) { 
+            //console.log(order + "is waiting for a message to pull");
+            var promise = new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    //console.log(order + "done waiting for a message for one second");
+                    resolve();
+                }, 1000);
             });
-
             await promise;
         }
+        this.emptySize++;
+        //console.log(order + "CAN DEQUEUE");
+    }
+}
+
+async function produce(publishers: Array<Publisher4>) {
+    while(true) {
+        for(let pub of publishers){
+            let tag = Math.floor(Math.random() * 3) + 1;
+            let content = Math.random().toString(36).substring(7);
+            let message = new Message4(tag, content);
+            pub.write(message);
+        }
+
+        let delayPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                //console.log("Publishers are stoping for 3 seconds");
+                resolve();
+            }, 3000);
+        });
+        await delayPromise;
+    }
+}
+
+async function consume(subscribers: Array<Subscriber4>) {
+    while(true) {
+        for(let sub of subscribers){
+            sub.read();
+        }
+
+        let delayPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                //console.log("Subscribers are stoping for 0.5 seconds");
+                resolve();
+            }, 500);
+        });
+        await delayPromise;
     }
 }
 
@@ -122,22 +200,34 @@ setInterval(() => { }, 1000); // run program until explicit exit
 
 (async () => {
     
-    var q : BoundedAsyncQueue = new BoundedAsyncQueue(3);
-    var p : Publisher4 = new Publisher4(q);
+    var p1 : Publisher4 = new Publisher4(3);
+    var p2 : Publisher4 = new Publisher4(3);
+    var p3 : Publisher4 = new Publisher4(3);
+    var p4 : Publisher4 = new Publisher4(3);
+    let publishers = [p1, p2, p3, p4];
 
-    var message1 : Message4 = new Message4(1, "message1");
-    var message2 : Message4 = new Message4(1, "message2");
-    var message3 : Message4 = new Message4(1, "message3");
-    var message4 : Message4 = new Message4(1, "message4");
+    var s1 : Subscriber4 = new Subscriber4([1], 5);
+    var s2 : Subscriber4 = new Subscriber4([2], 5);
+    var s3 : Subscriber4 = new Subscriber4([3], 5);
+    let subscribers = [s1, s2, s3];
 
-    p.write(message1);
-    p.write(message2);
-    p.write(message3);
-    await p.write(message4);
-    console.log("se isto não aparecer, é porque ainda esta à espera");
-    /*p.write(message2);
-    p.write(message3);
-    p.write(message4);*/
+    produce(publishers);
+
+    let broker = new Broker(publishers, subscribers);
+    broker.work();
+
+    consume(subscribers);
+
+    let delayPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Test finished");
+            resolve();
+        }, 5000);
+    });
+    await delayPromise;
+    //await s.read();
+    //console.log("se isto não aparecer, é porque ainda esta à espera que mensagens aparecam para ler");
+    
 
     /*var message = await p.queue.dequeue()
     console.log(message.content);*/
